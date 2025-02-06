57cf8bc6bf70f4d0e8be855b789f02bb8273c29c
Thribhu Krishnan <99516780+4tkbytes@users.noreply.github.com>
2025-02-06 08:00:59 +0000

    Uploaded completed indoor.py

---
+++ indoor/indoor.py
@@ -0,0 +1,12 @@
+# take an input
+# convert input into indoor voice using string lowercase function
+# print value
+
+print("Shush! Make it indoor voice")
+print("Input in your word: ")
+
+name = input()
+
+name = name.lower()
+
+print(name)
