f9cb5a644c11e376a47d8e8a9a3b7393f4b5dca8
Thribhu Krishnan <99516780+4tkbytes@users.noreply.github.com>
2025-02-06 08:23:36 +0000

    Uploaded completed playback.py

---
+++ playback/playback.py
@@ -0,0 +1,8 @@
+print("This is the 75100 machine that splits your input into 0.75!")
+print("Input: ")
+
+_input = input()
+
+# pretend input is "This is CS50"
+val = _input.replace(' ', '...')
+print(val)
