ce3cf97b9fcb40280de3a115085f84ea502e9fdf
Thribhu Krishnan <99516780+4tkbytes@users.noreply.github.com>
2025-02-16 12:11:46 +0000

    Uploaded completed nutrition.py

---
+++ 2-Loops/nutrition/nutrition.py
@@ -0,0 +1,25 @@
+fruit_calories = {
+    "apple": "130",
+    "avocado": "50",
+    "banana": "110",
+    "cantaloupe": "50",
+    "grapefruit": "60",
+    "grapes": "90",
+    "honeydew melon": "50",
+    "kiwi fruit": "90",
+    "lemon": "15",
+    "lime": "20",
+    "nectarine": "60",
+    "pear": "100",
+    "pineapple": "50",
+    "plums": "70",
+    "strawberries": "50",
+    "sweet cherries": "100",
+    "tangerine": "50",
+    "watermelon": "80"
+}
+
+_input = input("Input your fruit: ").lower()
+
+if _input in fruit_calories:
+    print("Calories: " + fruit_calories[_input])
