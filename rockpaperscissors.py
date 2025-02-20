7908c8bc6b40511249146e3f0d7fe6a3ad521dcc
Thribhu Krishnan <99516780+4tkbytes@users.noreply.github.com>
2025-02-20 22:41:17 +0000

    Added even more features

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -1,15 +1,12 @@
-# ask for input of rock paper scissors
-
 import random
 
-def compare_and_result(player: str, op: str) -> bool:
+def compare_and_result(player: str, op: str):
     # return true if player won, else return false is not
     
     print(f"Opponent did {op}...")
     
     if player == op:
-        print("Tie!")
-        return False
+        return
     
     match player:
         case 'rock':
@@ -29,29 +26,46 @@ def compare_and_result(player: str, op: str) -> bool:
                 return False
 
 def main():
-    while True:
-        player = input("Enter your option: [rock, paper, scissors] || ")
-        
-        options = ['rock', 'paper', 'scissors']
-        
-        if player not in options:
-            print("Make sure it is one of those options")
+    playerScore: int = 0
+    opponentScore: int = 0
+    
+    name = input("What is your name? ")
+    print(f"Welcome to the rock paper scissors game {name}. Enter CTRL+D to quit\n\n")
+    
+    try:
+        while True:
+            player = input(f"Enter your option {name}: [rock, paper, scissors] || ")
+            
+            options = ['rock', 'paper', 'scissors']
+            
+            if player not in options:
+                print("Make sure it is one of those options")
 
-        op = random.randint(1, 3)
-        newvar:str
-        match op:
-            case 1:
-                newvar = 'rock'
-            case 2:
-                newvar = 'paper'
-            case 3:
-                newvar = 'scissors'
-        
-        result = compare_and_result(player, newvar)
-        
-        if result == True:
-            print("You won!")
-        else:
-            print("You lost :(")
+            op = random.randint(1, 3)
+            newvar:str
+            match op:
+                case 1:
+                    newvar = 'rock'
+                case 2:
+                    newvar = 'paper'
+                case 3:
+                    newvar = 'scissors'
+            
+            print(f"DEBUG || Opponent did: {op}")
+            
+            result = compare_and_result(player, newvar)
+            
+            
+            if result is True:
+                print("You won!")
+                playerScore+=1
+            elif result is False:
+                print("You lost :(")
+                opponentScore+=1
+            else:
+                print("Tie, no points!")
+                
+    except EOFError:
+        print(f"\n\nTotal score: \n Player = {playerScore} \n Opponent = {opponentScore}")
 
 main()
